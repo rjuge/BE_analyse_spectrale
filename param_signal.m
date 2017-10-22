4030f6599e0f69ea55fec2ea683c72b3ed80ff2c
simonghrt <simon.gheeraert.sg@gmail.com>
2017-10-22 17:49:22 +0200

    Etape intermediaire pour param signal

--- param_signal.m
+++ param_signal.m
@@ -18,10 +18,20 @@ data_Demo_Stft = data_Demo(1:f_Subsampling:end,:);
 data_Length = length(data_Demo_Stft);
 data_Mat_Time_Axis = time_Sampling*([1:data_Length]-1)';
 
-p=5; % order of the AR algorithm
+p=15; % order of the AR algorithm
 % Voir méthodes de choix de p (vers page 335)
-
+for i =1:n
+    figure(i);
+    subplot(3,1,1);
+    mylevinsondurbin(data_Demo_Stft(:,i)', p, freq_Sampling)
+    hold on
+    subplot(3,1,2);
+    myburg(data_Demo_Stft(:,i), p, freq_Sampling)
+    hold on
+    subplot(3,1,3);
+    mymarple_matlab(data_Demo_Stft(:,i), p, freq_Sampling)
+end
 % Test for the different AR algorithms
-%mylevinsondurbin(data_Demo_Stft(:,1), p, freq_Sampling)
-myburg(data_Demo_Stft(:,1), p, freq_Sampling)
-mymarple_matlab(data_Demo_Stft(:,1), p, freq_Sampling)
+%mylevinsondurbin(data_Demo_Stft(:,1)', p, freq_Sampling)
+%myburg(data_Demo_Stft(:,1), p, freq_Sampling)
+%mymarple_matlab(data_Demo_Stft(:,1), p, freq_Sampling)
