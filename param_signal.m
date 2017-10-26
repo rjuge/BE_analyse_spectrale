c2017d29b86824730baa27719be378268bc30ba6
Simon Gheeraert <Simon.Gheeraert@student.centralesupelec.fr>
2017-10-26 19:03:55 +0200

    Test for param_signal

--- param_signal.m
+++ param_signal.m
@@ -18,20 +18,40 @@ data_Demo_Stft = data_Demo(1:f_Subsampling:end,:);
 data_Length = length(data_Demo_Stft);
 data_Mat_Time_Axis = time_Sampling*([1:data_Length]-1)';
 
-p=4; % order of the AR algorithm
+% Choses à faire
+% - Avec les mêmes signaux et pour chaque méthode, faire des plots avec
+% différentes valeurs de p
+% - Avec le même signal et même p (sufisammment grand il faudrait du coup), faire le triple plot (pas de superposé finalement)
+% + faire la même chose mais en faisant apparaître la freq max
+% - Faire comparaison des bruits pour chaque méthode et avec des p
+% différents
+
+p_arr_lev = [2,3,4,6];
+p_arr_burg = [2,4,8,16,32,64,128];
+
+% Plot several burg with different p values
+for i = 1:length(p_arr)
+    [aa1, sigma1, ref1, ff1, mydsp1] = mylevinsondurbin(data_Demo_Stft(:,i)', p_arr(i), freq_Sampling);
+end 
+
+
+p_levinson = 4; % order of the AR algorithm
+p_burg = 4;
+p_marple = 4;
 % Voir méthodes de choix de p (vers page 335)
 
-for i =1:n
-    figure(i);
-    subplot(3,1,1);
-    [aa1, sigma1, ref1, ff1, mydsp1] = mylevinsondurbin(data_Demo_Stft(:,i)', p, freq_Sampling);
-    hold on
-    subplot(3,1,2);
-    [aa2, sigma2, kk2, ff2, mydsp2] = myburg(data_Demo_Stft(:,i), p, freq_Sampling);
-    hold on
-    subplot(3,1,3);
-    [aa3, sigma3, kk3, ff3, mydsp3] = mymarple_matlab(data_Demo_Stft(:,i), p, freq_Sampling);
-end
+% Plot 4 signals with the three differents methods
+% for i =1:n
+%     figure(i);
+%     subplot(3,1,1);
+%     [aa1, sigma1, ref1, ff1, mydsp1] = mylevinsondurbin(data_Demo_Stft(:,i)', p, freq_Sampling);
+%     hold on
+%     subplot(3,1,2);
+%     [aa2, sigma2, kk2, ff2, mydsp2] = myburg(data_Demo_Stft(:,i), p, freq_Sampling);
+%     hold on
+%     subplot(3,1,3);
+%     [aa3, sigma3, kk3, ff3, mydsp3] = mymarple_matlab(data_Demo_Stft(:,i), p, freq_Sampling);
+% end
 
 
 % Test for the different AR algorithms
