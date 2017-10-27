db56caaea7e71937a0efaf44bc01d02105b69320
Simon Gheeraert <Simon.Gheeraert@student.centralesupelec.fr>
2017-10-27 17:47:43 +0200

    Plot each function

--- param_signal.m
+++ param_signal.m
@@ -26,21 +26,56 @@ data_Mat_Time_Axis = time_Sampling*([1:data_Length]-1)';
 % - Faire comparaison des bruits pour chaque méthode et avec des p
 % différents
 
-p_arr_lev = [2,3,4,6];
-p_arr_burg = [2,4,8,16,32,64,128];
+% ------------------------------------------------------------------------
 
-% Plot several burg with different p values
-for i = 1:length(p_arr)
-    [aa1, sigma1, ref1, ff1, mydsp1] = mylevinsondurbin(data_Demo_Stft(:,i)', p_arr(i), freq_Sampling);
-end 
+% 
+% Avec les mêmes signaux et pour chaque méthode, faire des plots avec
+% différentes valeurs de p
+%
+
+% Plot several levinson with different p values
+% p_arr_lev = [2,3,4,6,10,20];
+% for j = 1:length(p_arr_lev)
+%     for i= 1:n
+%         figure(i);
+%         [aa1, sigma1, ref1, ff1, mydsp1] = mylevinsondurbin(data_Demo_Stft(:,i)', p_arr_lev(j), freq_Sampling);
+%         legend('2','3','4','6','10','20');
+%     end 
+% end
 
 
-p_levinson = 4; % order of the AR algorithm
-p_burg = 4;
-p_marple = 4;
+% % Plot several burg with different p values
+% p_arr_burg = [2,4,8,16,32,64,128];
+% for j = 1:length(p_arr_burg)
+%     for i= 1:n
+%         figure(i);
+%         [aa2, sigma2, kk2, ff2, mydsp2] = myburg(data_Demo_Stft(:,i), p_arr_burg(j), freq_Sampling);
+%         % legend('2','3','4','6','10','20');
+%     end 
+% end
+
+% % Plot several burg with different p values
+% p_arr_marple = [2,4,8,16,32,64,128];
+% for j = 1:length(p_arr_marple)
+%     for i= 1:n
+%         figure(i);
+%         [aa3, sigma3, kk3, ff3, mydsp3] = mymarple_matlab(data_Demo_Stft(:,i), p_arr_marple(j), freq_Sampling);
+%         % legend('2','3','4','6','10','20');
+%     end 
+% end
+
+% ---------------------------------------------------------------------
+
+%
+% Avec le même signal et même p (sufisammment grand il faudrait du coup), faire le triple plot (pas de superposé finalement)
+% + faire la même chose mais en faisant apparaître la freq max
+%
+
 % Voir méthodes de choix de p (vers page 335)
 
+
 % Plot 4 signals with the three differents methods
+% p = 15;
 % for i =1:n
 %     figure(i);
 %     subplot(3,1,1);
@@ -54,7 +89,33 @@ p_marple = 4;
 % end
 
 
-% Test for the different AR algorithms
-%mylevinsondurbin(data_Demo_Stft(:,1)', p, freq_Sampling)
-%myburg(data_Demo_Stft(:,1), p, freq_Sampling)
-%mymarple_matlab(data_Demo_Stft(:,1), p, freq_Sampling)
+% Plot 4 signals with the three differents methods and p different for each
+% method
+% p_levinson = 4;
+% p_burg = 4;
+% p_marple = 4;
+% for i =1:n
+%     figure(i);
+%     subplot(3,1,1);
+%     [aa1, sigma1, ref1, ff1, mydsp1] = mylevinsondurbin(data_Demo_Stft(:,i)', p_levinson, freq_Sampling);
+%     hold on
+%     subplot(3,1,2);
+%     [aa2, sigma2, kk2, ff2, mydsp2] = myburg(data_Demo_Stft(:,i), p_burg, freq_Sampling);
+%     hold on
+%     subplot(3,1,3);
+%     [aa3, sigma3, kk3, ff3, mydsp3] = mymarple_matlab(data_Demo_Stft(:,i), p_marple, freq_Sampling);
+% end
+
+% Plot 4 signals with the three differents methods with the max frequency
+% p = 15;
+% for i =1:n
+%     figure(i);
+%     subplot(3,1,1);
+%     [aa1, sigma1, ref1, ff1, mydsp1] = mylevinsondurbin(data_Demo_Stft(:,i)', p, freq_Sampling);
+%     hold on
+%     subplot(3,1,2);
+%     [aa2, sigma2, kk2, ff2, mydsp2] = myburg(data_Demo_Stft(:,i), p, freq_Sampling);
+%     hold on
+%     subplot(3,1,3);
+%     [aa3, sigma3, kk3, ff3, mydsp3] = mymarple_matlab(data_Demo_Stft(:,i), p, freq_Sampling);
+% end
